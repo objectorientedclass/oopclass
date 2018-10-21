60df8eb02567f8ee27a68843ec51d631dc81d428
mahdi <mahdihadian@hotmail.com>
2018-10-21 23:40:46 +0330

    elham abdi

---
+++ elham abdi/week 1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23mehr
+{
+    class program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("name:Elham,Family:Abdi,ID_Number:0084447286,major:IT_IT,entrance:97_1");
+        }
+    }
+}

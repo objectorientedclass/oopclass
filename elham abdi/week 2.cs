dc2e38d6ff90fdee48887fc304ca8d2cf82a02b6
mahdi <mahdihadian@hotmail.com>
2018-10-28 23:19:09 +0330

    week 2

---
+++ elham abdi/week 2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class average
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            string name;
+          
+            float ave, P,E,M;
+            Console.Write("enter student's name and family");
+            name = Console.ReadLine();
+            Console.Write("enter student's ID number");
+            i = int.Parse(Console.ReadLine());
+            Console.Write("enter math mark");
+            M = float.Parse(Console.ReadLine());
+            Console.Write("enter programming mark");
+            P = float.Parse(Console.ReadLine());
+            Console.Write("enter english mark");
+            E = float.Parse(Console.ReadLine());
+            ave = (M + P + E) / 3;
+            Console.WriteLine("average of {0} is {1}", name, ave);
+            Console.ReadKey();
+            
+           
+     
+                
+                
+              
+
+
+
+        }
+    }
+}
---
+++ elham abdi/week 2_.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class average
+    {
+        static void Main(string[] a/*برنامه ای بنویسید کهشماره دانشجویی نام نام خانوادگی سه نمره راضی و برنامه نویسی و انگلیسی یک دانشجو را خوانده
+   
+          int ID;
+    
+        }
+
+    }
+}

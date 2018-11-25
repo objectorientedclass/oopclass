cdbcdbe34014112cadb9a923067dd7e0ded1f64d
mahdihadian <mahdihadian@hotmail.com>
2018-11-25 23:19:54 +0330

    5 azar 11:20PM

---
+++ elham abdi/week 5.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mostatildatatype
+{
+    class datatypetest
+    {
+        static void Main(string[] args)
+        {
+            int l, w;
+            Console.Write("enter lenghth");
+            l = int.Parse(Console.ReadLine());
+            Console.Write("enter width");
+            w = int.Parse(Console.ReadLine());
+            typetest(l,w);
+
+        }
+        static void typetest(int l)
+        {
+            int a, s;
+            a = l * w;
+            s = (l + w) / 2;
+            Console.WriteLine("masahat={0} mohit={1}", a, s);
+        }
+    }
+}
---
+++ elham abdi/week 5__.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibo_bazgashti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {int fibo(int n)
+            { if (n == 1 || n == 2) return 1;
+                else return fibo(n - 1) + fibo(n - 2);
+                Console.ReadKey();
+                    }
+        }
+    }
+}

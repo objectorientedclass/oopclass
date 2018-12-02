0c6f2726b85404caa35857638dbd3ca571e8594b
mahdihadian <mahdihadian@hotmail.com>
2018-12-02 23:50:45 +0330

    10azar 11:51PM

---
+++ zahra rezayi/week 6.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    using System;
+    public class date1
+    {
+        private int year;
+        private int month;
+        private int day;
+        public  date1()
+        {
+            setdate(0, 0, 0);
+        }
+        public void setdate(int yearvalue, int monthvalue, int dayvalue)
+    {
+
+        year = (yearvalue > 0) ? yearvalue : 0;
+
+        month = (monthvalue > 0 && monthvalue<= 12)? monthvalue:0;
+        day = (dayvalue > 0 && dayvalue <= 30) ? dayvalue : 0;
+    }
+        public string tostring()
+        {
+            return string.Format("{0}:{1}:{2}";year,month,day);
+        }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}

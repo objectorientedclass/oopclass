c5f177e7c6c65472f2627e6d46ba2982613d1705
mahdihadian <mahdihadian@hotmail.com>
2018-12-23 22:56:42 +0330

    1shanbe 10:57PM

---
+++ zahra rezayi/week 8.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_math_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            parallelogram p = new parallelogram(4,3,2);
+            Rectangle r = new Rectangle(3,2);
+            Squar s = new Squar(2, 2);
+            Console.WriteLine("Area:{0} Surrounding:{1}", p.CalculateArea(), p.Calculatesurrounding());
+            Console.WriteLine("Area:{0} Surrounding:{1}", r.CalculateArea(), r.Calculatesurrounding());
+            Console.WriteLine("Area:{0} Surrounding:{1}", s.CalculateArea(), s.Calculatesurrounding());
+            Console.ReadKey();
+        }
+    }
+}
